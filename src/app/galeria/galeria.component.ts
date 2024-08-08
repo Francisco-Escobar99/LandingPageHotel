6873123850c17273eb21bb61b48c08c223183983
Francisco-Escobar99 <escobarfrancisco9905@gmail.com>
2024-08-08 00:31:42 -0600

    GaleriaCompletada

--- src/app/galeria/galeria.component.ts
+++ src/app/galeria/galeria.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  ViewChild, ElementRef  } from '@angular/core';
 
 @Component({
   selector: 'app-galeria',
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GaleriaComponent implements OnInit {
 
+  @ViewChild('imagenesLight', { static: true }) imagenesLight!: ElementRef<HTMLImageElement>;
+  @ViewChild('contenedorLight', { static: true }) contenedorLight!: ElementRef<HTMLDivElement>;
+  @ViewChild('menu1', { static: true }) menu1!: ElementRef<HTMLDivElement>;
+  @ViewChild('closeIcon', { static: true }) closeIcon!: ElementRef<HTMLImageElement>;
+
+  imagenes: string[] = [
+    "./assets/images/imagen_1.jpg",
+    "./assets/images/imagen_2.jpg",
+    "./assets/images/imagen_3.jpg",
+    "./assets/images/imagen_4.jpg",
+    "./assets/images/imagen_5.jpg",
+    "./assets/images/imagen_6.jpg",
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onImageClick(src: string): void {
+    this.aparecerImagen(src);
+  }
+
+  onContainerClick(event: Event): void {
+    if (event.target !== this.imagenesLight.nativeElement) {
+      this.toggleLightbox();
+    }
+  }
+
+
+
+  private aparecerImagen(src: string): void {
+    this.imagenesLight.nativeElement.src = src;
+    this.toggleLightbox();
+  }
+
+  private toggleLightbox(): void {
+    this.contenedorLight.nativeElement.classList.toggle('show');
+    this.imagenesLight.nativeElement.classList.toggle('showImage');
+    this.menu1.nativeElement.style.opacity = this.menu1.nativeElement.style.opacity === '1' ? '0' : '1';
+  }
+
+  
 
 }
